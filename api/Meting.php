a0eea22ad009db14eadcec1772bc33f1d97c8ce5
liuhao5 <liuhao5@sensetime.com>
2023-11-05 15:54:35 +0800

    accessable version

--- api/Meting.php
+++ api/Meting.php
@@ -1535,3 +1535,5 @@ class Meting
     }
 
 }
+
+?>
--- api/index.php
+++ api/index.php
@@ -1,27 +1,31 @@
 <?php
-
-require 'vendor/autoload.php';
-// require 'Meting.php';
-
+require 'Meting.php';
 use Metowolf\Meting;
+$api = new Meting($_GET['server']);
+$data = $api->playlist($_GET['id']);
+echo $data
+
+// require 'vendor/autoload.php';
 
 // Initialize to netease API
-$api = new Meting('netease');
+// $api = new Meting('netease');
 
 // Use custom cookie (option)
 // $api->cookie('paste your cookie');
 
 // Get data
-$data = $api->format(true)->search('Soldier', [
-    'page' => 1,
-    'limit' => 50
-]);
+// $data = $api->format(true)->search('Soldier', [
+//     'page' => 1,
+//     'limit' => 50
+// ]);
 
-echo $data;
+// echo $data;
 // [{"id":35847388,"name":"Hello","artist":["Adele"],"album":"Hello","pic_id":"1407374890649284","url_id":35847388,"lyric_id":35847388,"source":"netease"},{"id":33211676,"name":"Hello","artist":["OMFG"],"album":"Hello",...
 
 // Parse link
-$data = $api->format(true)->url(35847388);
+// $data = $api->format(true)->url(35847388);
 
-echo $data;
+// echo $data;
 // {"url":"http:\/\/...","size":4729252,"br":128}
+
+?>
